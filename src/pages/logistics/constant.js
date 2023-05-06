54922a2ae2454d9abbf74cf76ffef187955a6f73
李泽胜 <zesheng.li@lanmaoly.com>
2023-05-06 15:11:38 +0800

    调整快递

---
+++ src/pages/logistics/constant.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: lizesheng
+ * @Date: 2023-04-12 17:53:10
+ * @LastEditors: lizesheng
+ * @LastEditTime: 2023-04-12 17:53:19
+ * @important: 重要提醒
+ * @Description: 备注内容
+ * @FilePath: /shop/src/pages/logistics/constant.js
+ */
+export const logisticsCompanies = [
+  { value: 'ZTO', label: '中通快递' },
+  { value: 'SF', label: '顺丰快递' },
+  { value: 'STO', label: '申通快递' },
+  { value: 'YTO', label: '圆通快递' },
+  { value: 'EMS', label: 'EMS' },
+  { value: 'JD', label: '京东快递' },
+];
