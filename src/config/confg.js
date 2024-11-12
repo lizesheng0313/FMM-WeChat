e905ef019e244bf63d7c7a5cd089b8bd0889d758
lizesheng <lizesheng0313@gmail.com>
2024-11-12 16:51:16 +0800

    feat: readme地址调整;

--- src/config/confg.js
+++ src/config/confg.js
@@ -1,7 +1,7 @@
 const constConfig = {
   title: '肥猫猫玩具商城',
-  staticHost: 'https://zjkdongao.com/images',
-  host: 'https://zjkdongao.com/qq',
+  staticHost: 'https://zjkdongao.cn/images',
+  host: 'https://zjkdongao.cn/qq',
   // host: 'http://127.0.0.1:7002',
 };
 
